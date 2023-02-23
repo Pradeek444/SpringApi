e91b20ccc6647e54c28c65eba004e284ff93e9c3
Pradeek444 <113882354+Pradeek444@users.noreply.github.com>
2023-02-23 11:35:09 +0530

    Add files via upload

---
+++ Day3_Post_Get/src/main/java/com/example/demo/Entity/Student.java
@@ -0,0 +1,50 @@
+package com.example.demo.Entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="Studentinformation")
+public class Student {
+    @Id
+    @Column(name="ID")
+    @GeneratedValue
+    private int id;
+    
+    @Column(name="Name")
+    @GeneratedValue
+    private String name;
+    
+    @Column(name="Dept")
+    @GeneratedValue
+    private String dept;
+    public Student() {}
+    Student(int id,String name,String dept){
+    	super();
+    	this.id=id;
+    	this.name=name;
+    	this.dept=dept;
+    }
+    //Getters,setters
+  public int getId() {			
+    return id;
+  }
+  public void setId(int id) {
+    this.id = id;
+  }
+  public String getName() {
+    return name;
+  }
+  public void setName(String name) {
+    this.name = name;
+  }
+  public String getDept() {
+    return dept;
+  }
+  public void setDept(String dept) {
+    this.dept = dept;
+   }
+}
